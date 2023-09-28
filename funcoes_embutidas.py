de5c8922b77dedd70214105ec7327e5be025d7e2
Vitor Ipiranga <vitorireboucas@edu.unifor.br>
2023-09-28 17:29:47 -0300

    Changes

--- funcoes_embutidas.py
+++ funcoes_embutidas.py
@@ -1,3 +1,10 @@
 lista = ['a', 'b', 'c', 'd', 'e', 'f']
 lista_strings_maiusculas = list(map(lambda x: x.upper(), lista))
 print(lista_strings_maiusculas)
+
+
+lista_palavras = ["banana", "maçã", "laranja", "abacaxi", "uva", "morango"]
+
+palavras_com_mais_de_5_letras = list(filter(lambda x: len(x) > 5, lista_palavras))
+
+print(palavras_com_mais_de_5_letras)
